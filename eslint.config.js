5e77a1ba98240f14cf8dd6616e619393305a7e05
Rikishini <taboopip@gmail.com>
2026-09-12 03:54:34 +0300

    Exclude agent worktrees from lint scope

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 
 export default [
-  { ignores: ["dist/**", "node_modules/**"] },
+  { ignores: ["dist/**", "node_modules/**", ".worktrees/**"] },
   js.configs.recommended,
   {
     files: ["**/*.{js,jsx,mjs}"],
